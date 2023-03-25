6c82bf34c59043fdfae21aef4e2152a7313101be
NubiaCarvalho <128197236+NubiaCarvalho@users.noreply.github.com>
2023-03-24 22:22:03 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,9 @@
+
+//Definindo uma lista
+let numeros = [2,4,6,8,10]
+
+//Percorrendo a lista
+  for (let = i; i < numeros.lenght; i++)(
+    if(numeros[i] % 2 === 0){
+     console.log(numero[i] + " é um, número par")
+    } else{ console.log(numero[numero[i] + "é um numero ímpar)
