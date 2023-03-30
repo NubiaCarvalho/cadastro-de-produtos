6dea9e8fe64d5dd2e979256ee92b7a4829cc9eef
NubiaCarvalho <128197236+NubiaCarvalho@users.noreply.github.com>
2023-03-30 14:10:54 -0300

    Update App.js

--- App.js
+++ App.js
@@ -2,8 +2,12 @@
 //Definindo uma lista
 let numeros = [2,4,6,8,10]
 
-//Percorrendo a lista
-  for (let = i; i < numeros.lenght; i++)(
-    if(numeros[i] % 2 === 0){
+//Percorrendo a lista com um loop for
+  for (let i = 0; i < numeros.lenght; i++) {
+    // Verificando se o número é par ou ímpar
+    if (numeros[i] % 2 === 0) {
      console.log(numero[i] + " é um, número par")
-    } else{ console.log(numero[numero[i] + "é um numero ímpar)
+    } else { 
+      console.log(numero[numero[i] + "é um numero ímpar");
+         } 
+  }                       
